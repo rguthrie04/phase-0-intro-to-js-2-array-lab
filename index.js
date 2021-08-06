c0af243537dd1e715c6b4b9212244d0f88be60da
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-06 11:15:40 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    return cats
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+    return cats
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats
+}
+function appendCat() {
+    appendCat = [...cats, "Broom"];
+    return appendCat;
+}
+function prependCat() {
+    prependCat = ["Arnold", ...cats];
+    return prependCat;
+}
+function removeLastCat() {
+    removeLastCat = cats.slice(0, cats.length -1);
+     return removeLastCat;
+}
+function removeFirstCat() {
+    removeFirstCat = cats.slice(1);
+    return removeFirstCat
+}
